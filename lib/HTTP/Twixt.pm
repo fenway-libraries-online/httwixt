04dfcaf8971a744a353156c011f8d88a7b8d792b
Paul Hoffman <nkuitse@nkuitse.com>
2016-10-20 20:47:02 -0400

    implement inetd, term, and CGI modes

--- lib/HTTP/Twixt.pm
+++ lib/HTTP/Twixt.pm
@@ -29,7 +29,9 @@ if (!defined caller) {
     my $cls;
     GetOptions(
         'd|daemon' => sub { $cls = __PACKAGE__ . '::Daemon' },
-        'i|inetd'  => sub { $cls = __PACKAGE__ . '::Inetd' },
+        'i|inetd'  => sub { $cls = __PACKAGE__ . '::Inetd'  },
+        'C|cgi'    => sub { $cls = __PACKAGE__ . '::CGI'    },
+        'F|fcgi'   => sub { $cls = __PACKAGE__ . '::FCGI'   },
         'c|config-file=s' => \$config_file,
         'u|uri-base=s' => \$uri_base,
         'r|root=s' => \$root,
@@ -39,6 +41,7 @@ if (!defined caller) {
         'v|verbose' => \$verbose,
     ) or die;
     $cls ||= 'HTTP::Twixt::' . (
+        -t STDERR    ? 'Term'   :
         $0 =~ /fcgi/ ? 'FCGI'   :
         $0 =~ /cgi/  ? 'CGI'    :
         $0 =~ /xtd$/ ? 'Daemon' :
@@ -64,6 +67,42 @@ sub new {
     bless { @_ }, $cls;
 }
 
+sub process {
+    my ($self) = @_;
+    my ($root, $pub, $prv, $red) = @$self{
+        qw(root public_dir private_dir redirect_status)
+    };
+    my ($hash, $url, $tpl) = eval { 
+        my $path = $self->read_request;
+        $self->publish($path);
+    };
+    if (defined $tpl) {
+        # Return HTML with the temporary URL embedded in it
+        my $html = $self->process_template(\$tpl, {
+            url => $url,
+            root => $root,
+            public => $pub,
+            private => $prv,
+            hash => $hash,
+        });
+        $self->send_response('200 OK', \$html);
+    }
+    elsif (defined $url) {
+        # Redirect to the desired resource
+        $self->send_response("$red Found", { Location => $url }, \'<html>Found</html>');
+    }
+    else {
+        # No such resource
+        $self->send_response('404 Not Found', \'<html>Not found</html>');
+    }
+}
+
+sub run {
+    my ($self) = @_;
+    $self->check_options;
+    $self->process;
+}
+
 sub check_options {
     my ($self) = @_;
     # Make sure we have absolute paths to the public and private directories
@@ -84,6 +123,7 @@ sub check_options {
 }
 
 sub read_config {
+    return {}
 }
 
 sub process_template {
@@ -102,7 +142,7 @@ sub publish {
     my ($coll, $file) = ($1, $2);
     die if !-e "$prv/$coll/$file";
     # Create a "random" URL and published the desired file
-    my $hash = digest();
+    my $hash = $self->digest;
     my ($src, $dst) = ("$prv/$coll", "$pub/$hash");
     die if !mkdir $dst;
     die if !symlink "$src/$file", "$dst/$file";
@@ -140,6 +180,21 @@ sub template {
     return $tpl;
 }
 
+sub make_header {
+    my $self = shift;
+    my ($status, $header, $cref) = @_;
+    my $clen = length $$cref;
+    my $ctype = 'text/html';
+    my @out = (
+        "Content-Type: $ctype",
+        "Content-Length: $clen",
+    );
+    while (my ($k, $v) = each %$header) {
+        push @out, "$k: $v";
+    }
+    return @out;
+}
+
 sub response_params {
     my $self = shift;
     my $status = shift;
@@ -157,42 +212,39 @@ sub response_params {
 
 # ------------------------------------------------------------------------------
 
+package HTTP::Twixt::Term;
+
+use base qw(HTTP::Twixt);
+
+sub read_request {
+    my ($self) = @_;
+    print STDERR "Path: " if -t STDIN;
+    my $path = <STDIN>;
+    print STDERR "\n" if -t STDIN;
+    die if !defined $path;
+    chomp $path;
+    return $path;
+}
+
+sub send_response {
+    my $self = shift;
+    my ($status, $header, $cref) = $self->response_params(@_);
+    my @out = (
+        "HTTP/1.0 $status",
+        $self->make_header($status, $header, $cref),
+        '',
+    );
+    print $_, "\n" for @out;
+    print $$cref;
+}
+
+# ------------------------------------------------------------------------------
+
 package HTTP::Twixt::Inetd;
 
 use base qw(HTTP::Twixt);
 
 use HTTP::Request;
-
-sub run {
-    my ($self) = @_;
-    $self->check_options;
-    my ($root, $pub, $prv, $red) = @$self{
-        qw(root public_dir private_dir redirect_status)
-    };
-    my ($hash, $url, $tpl) = eval { 
-        my $path = $self->read_request;
-        $self->publish($path);
-    };
-    if (defined $tpl) {
-        # Return HTML with the temporary URL embedded in it
-        my $html = $self->process_template(\$tpl, {
-            url => $url,
-            root => $root,
-            public => $pub,
-            private => $prv,
-            hash => $hash,
-        });
-        $self->send_response('200 OK', \$html);
-    }
-    elsif (defined $url) {
-        # Redirect to the desired resource
-        $self->send_response("$red Found", { Location => $url }, \'<html>Found</html>');
-    }
-    else {
-        # No such resource
-        $self->send_response('404 Not Found', \'<html>Not found</html>');
-    }
-}
 
 sub read_request {
     my ($self) = @_;
@@ -207,17 +259,11 @@ sub read_request {
 sub send_response {
     my $self = shift;
     my ($status, $header, $cref) = $self->response_params(@_);
-    my $clen = length $$cref;
-    my $ctype = 'text/html';
     my @out = (
         "HTTP/1.0 $status",
-        "Content-Type: $ctype",
-        "Content-Length: $clen",
+        $self->make_header($status, $header, $cref),
+        '',
     );
-    while (my ($k, $v) = each %$header) {
-        push @out, "$k: $v";
-    }
-    push @out, '';
     print $_, "\x0d\x0a" for @out;
     print $$cref;
 }
@@ -230,15 +276,38 @@ use base qw(Net::Server::HTTP HTTP::Twixt);
 
 sub run {
     my ($self) = @_;
-    eval "use Net::Server::HTTP" or die;
+    eval "use Net::Server::HTTP; 1" or die;
     my $config = $self->read_config;
-    $self->run(
+    $config->{port} ||= 9999;
+    $self->check_options;
+    $self->SUPER::run(
         %$config,
         -t STDIN ? (server_type => 'Single') : (),
     );
 }
 
+sub read_request {
+    eval "use CGI; 1" or die;
+    my ($self) = @_;
+    my $q = $self->{'cgi'} = CGI->new;
+    return $q->path_info || '/';
+}
+
 sub process_http_request {
+    my ($self) = @_;
+    $self->process;
+}
+
+sub send_response {
+    my $self = shift;
+    my ($status, $header, $cref) = $self->response_params(@_);
+    my @out = (
+        "HTTP/1.0 $status",
+        $self->make_header($status, $header, $cref),
+        '',
+    );
+    print $_, "\n" for @out;
+    print $$cref;
 }
 
 # ------------------------------------------------------------------------------
@@ -252,6 +321,25 @@ use base qw(HTTP::Twixt);
 package HTTP::Twixt::CGI;
 
 use base qw(HTTP::Twixt);
+
+sub read_request {
+    eval "use CGI; 1" or die;
+    my ($self) = @_;
+    my $q = $self->{'cgi'} = CGI->new;
+    return $q->path_info || '/';
+}
+
+sub send_response {
+    my $self = shift;
+    my ($status, $header, $cref) = $self->response_params(@_);
+    my @out = (
+        "Status: $status",
+        $self->make_header($status, $header, $cref),
+        '',
+    );
+    print $_, "\x0d\x0a" for @out;
+    print $$cref;
+}
 
 # ------------------------------------------------------------------------------
 
